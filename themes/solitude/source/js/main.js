8f2154cb686419dabc52bbe66c99230b6ed354e8
zhangjiuxiu10086 <148981544+zhangjiuxiu10086@users.noreply.github.com>
2026-01-10 19:24:09 +0800

    Update main.js

--- themes/solitude/source/js/main.js
+++ themes/solitude/source/js/main.js
@@ -907,7 +907,7 @@ window.refreshFn = () => {
   const { is_home, is_page, page, is_post, ai_text } = PAGE_CONFIG;
   const { runtime, lazyload, lightbox, randomlink, covercolor, lure, expire } =
     GLOBAL_CONFIG;
-  const timeSelector = ".webinfo-item time";
+  const timeSelector = ".datetime, .webinfo-item time, .post-meta-date time";
   document.body.setAttribute("data-type", page);
   sco.changeTimeFormat(document.querySelectorAll(timeSelector));
   runtime && sco.addRuntime();
